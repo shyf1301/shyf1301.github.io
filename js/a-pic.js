49b7261ad891c6f31629181cf238cf2ed2b690c1
shyf1301 <454246635@qq.com>
2022-07-25 21:00:17 +0800

    Site updated: 2022-07-25 21:00:17

--- js/a-pic.js
+++ js/a-pic.js
@@ -1,22 +1,43 @@
 
-$.ajax({
-  type: 'GET',
+// $.ajax({
+//   type: 'GET',
+//   headers: {
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Method': 'GET,POST'
+//   },
+//   url: 'https://dev.iw233.cn/api.php?sort=random&type=json&num=40',
+//   timeout: 10000,
+//   success: function(data) {
+//       const pics = data.pic
+//       for (let index = 0; index < pics.length; index++) {
+//         const element = document.getElementById('pic' + (index + 1));
+//         if (element) element.src = pics[index]
+//       }
+//   },
+//   error: function(a, b, c) {
+//       console.log(a)
+//       console.log(b)
+//       console.log(c)
+//   }
+// })
+
+const promise = fetch('https://dev.iw233.cn/api.php?sort=random&type=json&num=40', {
+  method: "GET",
   headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Method': 'GET,POST'
-  },
-  url: 'https://dev.iw233.cn/api.php?sort=random&type=json&num=40',
-  timeout: 10000,
-  success: function(data) {
-      const pics = data.pic
-      for (let index = 0; index < pics.length; index++) {
-        const element = document.getElementById('pic' + (index + 1));
-        if (element) element.src = pics[index]
-      }
-  },
-  error: function(a, b, c) {
-      console.log(a)
-      console.log(b)
-      console.log(c)
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+  }
+})
+.then(data => {
+  return data.json()
+})
+.catch(error => {return error})
+
+promise.then(data => {
+  const pics = data.pic
+  for (let index = 0; index < pics.length; index++) {
+    const element = document.getElementById('pic' + (index + 1));
+    if (element) element.src = pics[index]
   }
 })
