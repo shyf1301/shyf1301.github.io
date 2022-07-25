75d3f3e98b5e949cb900745477185fb1b781cb6e
shyf1301 <454246635@qq.com>
2022-07-25 20:34:40 +0800

    Site updated: 2022-07-25 20:34:40

--- js/a-pic.js
+++ js/a-pic.js
@@ -2,7 +2,8 @@
 $.ajax({
   type: 'GET',
   headers: {
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Referrer': ''
   },
   url: 'https://dev.iw233.cn/api.php?sort=random&type=json&num=40',
   timeout: 10000,
